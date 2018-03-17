0a27023bbca987aae8537ad2776ec18ac0f20254
Peter Bingham <petersbingham@hotmail.co.uk>
2018-03-17 11:08:55 +0000

    Copy test file and README on installation.

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 
 from distutils.core import setup
+import shutil
+shutil.copy('README.md', 'TwoChanRadialWell/README.md')
 
 setup(name='TwoChanRadialWell',
-      version='0.4',
+      version='0.5',
       description='Calculates solutions to the two channel radial well as described in Newton\'s "Scattering Theory of Waves and Particles".',
       author="Peter Bingham",
       author_email="petersbingham@hotmail.co.uk",
       packages=['TwoChanRadialWell'],
+      package_data={'TwoChanRadialWell': ['tests/*', 'README.md'],
      )
